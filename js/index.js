c92a4c975ae56de072d93374fed996f821966c2c
Danil <jetnt123456@gmail.com>
2022-07-11 12:23:38 +0300

    Little change
    Changing design and adding likes

--- js/index.js
+++ js/index.js
@@ -2,12 +2,13 @@ let vue = new Vue({
   el: '#app',
   data: {
     names: [
-      {title: "Go?"},
-      {title: "Dune"},
-      {title: "Dune Timoti love"},
-      {title: "Dune Love is"},
-      {title: "Zzz"},
-      {title: "Tribute"},
+      {title: "Go?", likes: 5},
+      {title: "Dune", likes: 105},
+      {title: "Dune Timoti love", likes: 15},
+      {title: "Dune Love is", likes: 150090},
+      {title: "Zzz", likes: 90},
+      {title: "Tribute", likes: 108900},
+      {title: "Doom", likes: 10},
     ]
   }
 });
